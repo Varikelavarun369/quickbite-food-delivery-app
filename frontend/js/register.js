7b5446e735304cf895c75d5102c0b12a193c0a8f
Varikela Varun <varikelavarun@gmail.com>
2026-06-27 19:00:02 +0530

    Update API endpoint for user registration

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -39,7 +39,7 @@ document
                 const response =
                     await fetch(
 
-                        "http://localhost:8080/users",
+                        "https://quickbite-food-delivery-app-xsz4.onrender.com/users",
 
                         {
 
